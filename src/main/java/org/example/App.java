781b10048d4bfc44a53f560d4ef00b8ada8f0476
saichethanr <saichethan9019@gmail.com>
2024-08-04 17:33:48 +0530

    2 beans for same class---bean without an id---2 references for the same bean

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,11 +11,15 @@ public class App
 
         //note that beans are the objects that are managed by the spring framework
         //this is not a part of java its a part of spring framework
-
+        //object is getting created on the line 15 itself
+        //all the classes mentioned as beans will be created as the statement below gets executed
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         //create the spring.xml passed above in the parameter and configure its declaration and then add breans to the classes the
         //which has to be created by the spring framework
-        Alien obj1 = (Alien) context.getBean("alien");
+        Alien obj1 = (Alien) context.getBean("alien1");
         obj1.code();
+
+        Alien obj2 = (Alien) context.getBean("alien1");
+        obj2.code();
     }
 }
