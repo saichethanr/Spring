7e916c64fab367c05816c443d9df51e64bde6fd3
saichethanr <saichethan9019@gmail.com>
2024-08-04 17:21:36 +0530

    xml configoration for beans achived

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        Alien  obj = new Alien();
+        obj.code();
+        //note that beans are the objects that are managed by the spring framework
+        //this is not a part of java its a part of spring framework
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        //create the spring.xml passed above in the parameter and configure its declaration and then add breans to the classes the
+        //which has to be created by the spring framework
+        Alien obj1 = (Alien) context.getBean("alien");
+        obj1.code();
+    }
+}
