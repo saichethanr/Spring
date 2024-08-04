e17aa4ac2a6bbddebcbccc0620801714c716a4a6
saichethanr <saichethan9019@gmail.com>
2024-08-04 22:29:28 +0530

    autowiring byname and bytype--bytype single bean must be present

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -5,24 +5,16 @@ import java.beans.ConstructorProperties;
 public class Alien {
     private int k ;
     private Computer comp;
-    private Laptop lap = new Laptop();
+
 
     //sprcified in the spring.xml file
     @ConstructorProperties({"age","lap"})
     public Alien(int k,Computer com){
         System.out.println("constructor for the k and lap variable called");
          this.k = k;
-         this.lap = lap;
+         this.comp = com;
     }
 
-
-    public Laptop getLap() {
-        return lap;
-    }
-
-    public void setLap(Laptop lap) {
-        this.lap = lap;
-    }
 
     public Computer getComp() {
         return comp;
