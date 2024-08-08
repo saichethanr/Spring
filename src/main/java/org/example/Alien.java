9983980545835e63c72998aa89c9a28f7e5e9d96
saichethanr <saichethan9019@gmail.com>
2024-08-08 11:33:11 +0530

    Value Annotation and Scope annotaion into the @Component

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -2,12 +2,14 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.beans.ConstructorProperties;
 
 @Component
 public class  Alien {
+    @Value("21")
     private int k ;
 
     //as by using the @component aspect we have notjing to write in the config files
--- src/main/java/org/example/Desktop.java
+++ src/main/java/org/example/Desktop.java
@@ -1,10 +1,12 @@
 package org.example;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("desk")
 @Primary
+@Scope("prototype")
 public class Desktop implements Computer{
 
 
